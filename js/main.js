7ec79e1bc9afe634a72e7c299a66f0de5c9dfbc8
Emanuela91 <emanuela.marson91@gmail.com>
2022-11-16 18:10:53 +0100

    aggiunto cambio colore box e numero in console

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
 // Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
 
 const contenitore = document.querySelector(".container");
 for (let i = 1; i <= 100; i++){
@@ -8,4 +10,12 @@ for (let i = 1; i <= 100; i++){
     element.classList.add("box");
     contenitore.append(element);
     element.append(i);
+
+    element.addEventListener("click",
+        function(){
+            element.classList.toggle("clicked");
+            console.log(i);
+        }
+        
+    );
 }
